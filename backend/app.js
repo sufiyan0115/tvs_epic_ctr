c6ad1860af1945a1b6607c622203295605dc4b72
Tanwir Ahmad <tanwirahmad2912@gmail.com>
2022-10-29 16:20:19 +0530

    Added Model

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,6 @@ const io = require("socket.io")(server, {
 
 io.on("connection", (socket) => {
   console.log("A user connected");
-  //Whenever someone disconnects this piece of code executed
   socket.on("disconnect", (socket) => {
     console.log("A user disconnected");
   });
